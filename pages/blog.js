80210ec73b3d3526136564af6166a875ab1340f6
fulln <245193247@qq.com>
2020-08-03 20:18:23 +0800

    :runner: add example

--- pages/blog.js
+++ pages/blog.js
@@ -11,17 +11,20 @@ export async function getStaticProps() {
 }
 
 export default function Blog({ allPostsData }) {
+  debugger
   return (
     <div>
         <ul >
-          {allPostsData.map(({ id, date, title }) => (
+          {allPostsData.map(({ id, content,date, title }) => (
             <li key={id}>
               {title}
               <br />
               {id}
               <br />
               {date}
-            </li>
+              <br/>
+              {content}    
+            </li>            
           ))}
         </ul>
     </div>
